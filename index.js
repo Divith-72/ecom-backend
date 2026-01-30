e9ed0282b70206296ac67b6b164a022ed550a429
Divith-72 <divith.s2024eee@sece.ac.in>
2026-01-30 21:56:29 +0530

    FoodLoop backend

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// FoodLoop – A Full-Stack MERN Online Food Ordering System (Backend)
 
 const express = require("express");
 const CartRoute = require("./routes/cartRoute");
@@ -11,15 +10,14 @@ connectdb();
 
 const app = express();
 
-// Middlewares
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use("/api", CartRoute);
 app.use("/api", FoodRoute);
 
-// Server start
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🍔 FoodLoop Backend Server is running on port ${PORT}`);
